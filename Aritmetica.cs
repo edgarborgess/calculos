f5b947f88d477bcc1a6eed692e9a51c840897062
Edgar Borges <157116836+edgarborgess@users.noreply.github.com>
2024-10-27 11:56:13 +0000

    comentários

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -19,6 +19,12 @@ namespace Calculos
         /// <returns></returns>
         public int Somar(int x, int y)
             { return x * y; }
+        /// <summary>
+        /// Efetua a subtração de dois valores
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public int Subtrair(int x, int y)
             { return x * y; }
     }
