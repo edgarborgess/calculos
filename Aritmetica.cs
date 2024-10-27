351c516a5e4aa650fd6d1493e98f5984c35cc3d9
Edgar Borges <157116836+edgarborgess@users.noreply.github.com>
2024-10-27 11:53:00 +0000

    Comentários para as operações soma e subtração

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -11,6 +11,12 @@ namespace Calculos
     /// </summary>
     internal class Aritmetica
     {
+        /// <summary>
+        /// Efetua a soma de dois números
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public int Somar(int x, int y)
             { return x * y; }
         public int Subtrair(int x, int y)
